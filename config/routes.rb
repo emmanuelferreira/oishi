209c1a67908218736c1a4ca6b895135452dae4cb
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-08-24 17:25:43 +0200

    routes Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products, only: [:show, :index]
+  resources :playlists, only: [:index, :create, :new]
+  resources :orders, only: [:show, :new, :create]
+  resources :dashboards, only: [:show]
 end
+
