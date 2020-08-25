cc03bf2d31b3ebe014198bcdd6a5f86cc5296acd
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-08-25 10:23:42 +0200

    model Subcategory creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_143448) do
+ActiveRecord::Schema.define(version: 2020_08_25_082054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,14 @@ ActiveRecord::Schema.define(version: 2020_08_24_143448) do
 
   create_table "shopping_preferences", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subcategories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "picture"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
