1eae087a63886bac982d15928b746998a9a1722f
Vincent Clair <vincentclair@hotmail.fr>
2020-08-25 17:51:13 +0200

    tabs update1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import "../components/tabs.js";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
