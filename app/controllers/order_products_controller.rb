2acdafe1a88997dd502dae9b7e83b5388f0fdf83
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-27 18:05:57 +0200

    delete creation

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -20,32 +20,14 @@ class OrderProductsController < ApplicationController
   end
 
   def delete_from_cart
-    if @current_cart.key?(order_product_params[:product_id])
-      @current_cart.reject{ |product_id, quantity| product_id.to_i == order_product_params[:product_id].to_i }
+    if @current_cart.key?(params[:product_id])
+      @current_cart.delete(params[:product_id])
     end
     session[:cart] = @current_cart
     render "create"
   end
 
-  def save
-    @order = Order.new(
-      status: "pending",
-      user_id: @current_user.id,
-      deliver_date: Date.today + 1,
-      address_id: @current_user.address.id,
-      total: @cart_amount
-      )
-    @order.save
 
-    @current_cart.each do |product_id, quantity|
-      order_product = OrderProduct.new(
-        product_id: product_id,
-        quantity: quantity,
-        unit_price: Product.find(product_id).price,
-        order_id: @order.id
-        )
-      order_product.save
-    end
 
   end
 
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,23 @@
 class OrdersController < ApplicationController
-  def create
+
+  def save
     @order = Order.new(
-    status: "pending",
-    user_id:
-    deliver_date:,
-    address_id:,
-    total:
-  end
+      status: "pending",
+      user_id: @current_user.id,
+      deliver_date: Date.today + 1,
+      address_id: @current_user.address.id,
+      total: @cart_amount
+      )
+    @order.save
+
+    @current_cart.each do |product_id, quantity|
+      order_product = OrderProduct.new(
+        product_id: product_id,
+        quantity: quantity,
+        unit_price: Product.find(product_id).price,
+        order_id: @order.id
+        )
+      order_product.save
+   end
+
 end
