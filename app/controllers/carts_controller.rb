260a81853d85d4aed94eac49b4535ae64f294586
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-27 14:53:23 +0200

    created new branch

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,8 @@
 class CartsController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index
+  def show
     @cart = @current_cart
-    @order_items = OrderProduct.all
   end
 
 end
