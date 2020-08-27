3067fdbd54f30f11a103db6bd6a4b41a9f3abf80
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-27 08:54:56 +0200

    ...

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @products = Product.all
+    @products = Product.includes(:subcategory)
     @suppliers = Supplier.all
     @order_product = @current_cart.order_products.new
   end
