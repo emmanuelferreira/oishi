d3dc26ab474311d485d63e3d878de200973c469b
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-28 11:23:46 +0200

    carges create method update to add Order and Order_Product creation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,8 +3,32 @@ class ChargesController < ApplicationController
   end
 
   def create
+     @cart_amount = @current_cart.inject(0) do |sum, (key, quantity)|
+           @product = Product.find(key)
+           sum += @product.price * quantity
+         end
+
+     @order = Order.new(
+       status: "pending",
+       user_id: @current_user.id,
+       deliver_date: Date.today + 1,
+       address_id: @current_user.address.id,
+       total: @cart_amount
+       )
+     @order.save
+
+     @current_cart.each do |product_id, quantity|
+       order_product = OrderProduct.new(
+         product_id: product_id,
+         quantity: quantity,
+         unit_price: Product.find(product_id).price,
+         order_id: @order.id
+         )
+       order_product.save
+    end
+
     # Amount in cents
-    @amount = 500
+    @amount = @cart_amount * 100
 
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
@@ -22,4 +46,7 @@ class ChargesController < ApplicationController
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  @order.status = "In progress"
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :address
   has_many :order_products
-  accepts_nested_attributes_for :order_products
 end
