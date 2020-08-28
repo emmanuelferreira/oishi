4195473019b6bf0a4fbfb75f22feaa6d8f86e8c1
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-28 13:49:45 +0200

    stripe create working

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,30 +1,39 @@
 class ChargesController < ApplicationController
   def new
+    @order_products = []
+    @current_cart.each do |product_id, quantity|
+      order_product = OrderProduct.new(
+        product_id: product_id,
+        quantity: quantity,
+        unit_price: Product.find(product_id).price
+        )
+      @order_products << order_product
+    end
   end
 
   def create
-     @cart_amount = @current_cart.inject(0) do |sum, (key, quantity)|
-           @product = Product.find(key)
-           sum += @product.price * quantity
-         end
+   @cart_amount = @current_cart.inject(0) do |sum, (key, quantity)|
+      @product = Product.find(key)
+      sum += @product.price * quantity
+    end
 
-     @order = Order.new(
-       status: "pending",
-       user_id: @current_user.id,
-       deliver_date: Date.today + 1,
-       address_id: @current_user.address.id,
-       total: @cart_amount
+   @order = Order.new(
+     status: "Payment not accepted",
+     user_id: current_user.id,
+     deliver_date: Date.today + 1,
+     address_id: current_user.address.id,
+     total: @cart_amount
+    )
+   @order.save!
+
+   @current_cart.each do |product_id, quantity|
+      order_product = OrderProduct.new(
+        product_id: product_id,
+        quantity: quantity,
+        unit_price: Product.find(product_id).price,
+        order_id: @order.id
        )
-     @order.save
-
-     @current_cart.each do |product_id, quantity|
-       order_product = OrderProduct.new(
-         product_id: product_id,
-         quantity: quantity,
-         unit_price: Product.find(product_id).price,
-         order_id: @order.id
-         )
-       order_product.save
+      order_product.save!
     end
 
     # Amount in cents
@@ -42,11 +51,16 @@ class ChargesController < ApplicationController
       currency: 'chf',
     })
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+
+    @order.status = "Pending"
+    @current_cart = {}
+    redirect_to dashboard_path, notice: 'Payment approved!'
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+
   end
 
-  @order.status = "In progress"
-
 end
+
