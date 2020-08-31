052de65a4bd12dd17055b77ff8015d3e97090072
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-08-31 16:14:41 +0200

    add calculations to attribute price icon color to products in index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require chartkick
-//= require Chart.bundle
-//= require highcharts
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -40,5 +37,4 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initTabSelector();
 });
