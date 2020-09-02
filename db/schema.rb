cb91ed190f35f6a8592ac8f8c7b0ecc4b68b4e20
Vincent Clair <vincentclair@hotmail.fr>
2020-09-02 09:39:04 +0200

    change payment layout

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,27 @@ ActiveRecord::Schema.define(version: 2020_09_01_152329) do
   enable_extension "hstore"
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "city"
@@ -166,6 +187,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_152329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "inventories", "products"
   add_foreign_key "inventories", "suppliers"
   add_foreign_key "order_products", "orders"
